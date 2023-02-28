ad9d7dcd7fe02bf861cd3c3fdd7701be1fed3e19
Rajvansh <106300079+steve-369@users.noreply.github.com>
2023-02-28 17:12:43 +0530

    build

---
+++ build/precache-manifest.dffb4964f5b60885c3d2aec2258b4aed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "284033f44f8b8a9af390",
+    "url": "/project/todoReact/build/static/css/main.6762d033.chunk.css"
+  },
+  {
+    "revision": "284033f44f8b8a9af390",
+    "url": "/project/todoReact/build/static/js/main.284033f4.chunk.js"
+  },
+  {
+    "revision": "3bc8ac50b02e373cc067",
+    "url": "/project/todoReact/build/static/js/1.3bc8ac50.chunk.js"
+  },
+  {
+    "revision": "32c54b0683b8c16568b0",
+    "url": "/project/todoReact/build/static/js/2.32c54b06.chunk.js"
+  },
+  {
+    "revision": "a99312468fbcf092f003",
+    "url": "/project/todoReact/build/static/js/runtime~main.a9931246.js"
+  },
+  {
+    "revision": "5f540bed1c34adfe68ebee5e05ad4269",
+    "url": "/project/todoReact/build/index.html"
+  }
+];
